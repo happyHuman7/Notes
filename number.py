e6c3c765b8100c39b092fe0135d1cc345dbb84f0
Inika <79232073+happyHuman7@users.noreply.github.com>
2025-05-20 17:43:07 -0400

    Update number.py

--- number.py
+++ number.py
@@ -1,44 +1,60 @@
-import random
+# import the streamlit library
+import streamlit as st
 
-print 
-print ("Welcome to the game! Lets see how good your number guessing skills are")
-    
-highNumb = int(input("What is the highest value in the range starting from 0? "))
-striker = int(input("How many chances do you want to get? "))
+# give a title to our app
+st.title('Welcome to BMI Calculator')
 
-print()
+# TAKE WEIGHT INPUT in kgs
+weight = st.number_input("Enter your weight (in kgs)")
 
-x = (random.randrange(0,highNumb))
+# TAKE HEIGHT INPUT
+# radio button to choose height format
+status = st.radio('Select your height format: ',
+                  ('cms', 'meters', 'feet'))
 
-wrongs = int(0)
-status = "in play"
+# compare status value
+if(status == 'cms'):
+    # take height input in centimeters
+    height = st.number_input('Centimeters')
 
-def checker (y):
-    global wrongs
-    global status
-    
-    if wrongs == striker:
-        print ("Yikes! You've striked out. The number was " + str(x))
-        status = "out"
-    elif y == x:
-        status = "win"
-        print ("Yay you won!")
-    elif y < x:
-        wrongs += 1
-        print ("Go higher. Strikes = " + str(wrongs))
-    elif y > x:
-        wrongs += 1
-        print("Go lower. Strikes = " + str(wrongs))
-    print ()
+    try:
+        bmi = weight / ((height/100)**2)
+    except:
+        st.text("Enter some value of height")
 
-while (wrongs < striker) or (status == "in play"):
-    guess = int(input ("Guess! "))
-    checker (guess)
+elif(status == 'meters'):
+    # take height input in meters
+    height = st.number_input('Meters')
 
-if wrongs == striker:
-    print ("HAHA you lost! The number was " + str(x))
-elif status == "win":
-    print (" :) ")
+    try:
+        bmi = weight / (height ** 2)
+    except:
+        st.text("Enter some value of height")
 
+else:
+    # take height input in feet
+    height = st.number_input('Feet')
 
+    # 1 meter = 3.28
+    try:
+        bmi = weight / (((height/3.28))**2)
+    except:
+        st.text("Enter some value of height")
 
+# check if the button is pressed or not
+if(st.button('Calculate BMI')):
+
+    # print the BMI INDEX
+    st.text("Your BMI Index is {}.".format(bmi))
+
+    # give the interpretation of BMI index
+    if(bmi < 16):
+        st.error("You are Extremely Underweight")
+    elif(bmi >= 16 and bmi < 18.5):
+        st.warning("You are Underweight")
+    elif(bmi >= 18.5 and bmi < 25):
+        st.success("Healthy")
+    elif(bmi >= 25 and bmi < 30):
+        st.warning("Overweight")
+    elif(bmi >= 30):
+        st.error("Extremely Overweight")
